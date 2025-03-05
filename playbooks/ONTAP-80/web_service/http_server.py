6b723638a4a794fb0b0ea1ee23a5b68a57b7eee8
AdrianBronder <45482283+AdrianBronder@users.noreply.github.com>
2025-03-05 17:11:21 +0100

    adding vault file

--- playbooks/ONTAP-80/web_service/http_server.py
+++ playbooks/ONTAP-80/web_service/http_server.py
@@ -373,9 +373,10 @@ def modify_service():
 
     r = ansible_runner.run(
         playbook=project_root_path+'/playbooks/ONTAP-01/ONTAP-01-04.yml',
-        inventory=inventory_path
+        inventory=inventory_path,
+        cmdline=f"--vault-password-file {project_root_path+'/init/init_helper/vaultfile.txt'}"
     )
-    
+
     if r.rc == 0:
         logger.info(f"Playbook executed successfully.")
     else:
