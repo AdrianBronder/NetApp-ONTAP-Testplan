a560f2780e1aeed92c83fe68d022238dccefd22d
AdrianBronder <45482283+AdrianBronder@users.noreply.github.com>
2025-04-25 10:35:01 -0400

    doc adjustments + writing s3 credentials to files

--- init/init_eapontap9161.sh
+++ init/init_eapontap9161.sh
@@ -41,7 +41,7 @@ dnf -y update
 echo ""
 echo ""
 echo "--> Installing additional packages"
-dnf install -y perl openssl-devel bzip2-devel zlib-devel sqlite-devel python3.11-kerberos krb5-workstation krb5-devel python3.11-pip python3.11-devel nfs-utils java-21-openjdk --skip-broken
+dnf install -y perl openssl-devel bzip2-devel zlib-devel sqlite-devel python3.11-kerberos krb5-workstation krb5-devel python3.11-pip python3.11-devel nfs-utils java-21-openjdk awscli --skip-broken
 
 echo ""
 echo ""
@@ -66,7 +66,7 @@ python3 -m pip install --upgrade pip
 echo ""
 echo ""
 echo "--> Installing additional Python packages"
-python3 -m pip install --upgrade requests six netapp_lib selinux flask Flask-LDAP3-Login jmespath pyyaml netapp_ontap
+python3 -m pip install --upgrade requests six netapp_lib selinux flask Flask-LDAP3-Login jmespath pyyaml netapp_ontap botocore boto3
 python3 -m pip install --upgrade "pywinrm[kerberos]>=0.3.0"
 
 echo ""
