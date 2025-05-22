d2a28b1cf5c9651be4c9408ec4141cbc3a980973
AdrianBronder <45482283+AdrianBronder@users.noreply.github.com>
2025-05-22 08:52:52 -0400

    final fixing after merges

--- init/init_eapontap9161.sh
+++ init/init_eapontap9161.sh
@@ -66,7 +66,7 @@ python3 -m pip install --upgrade pip
 echo ""
 echo ""
 echo "--> Installing additional Python packages"
-python3 -m pip install --upgrade requests six netapp_lib selinux flask Flask-LDAP3-Login jmespath pyyaml netapp_ontap botocore boto3
+python3 -m pip install --upgrade requests six netapp_lib selinux flask Flask-LDAP3-Login jmespath pyyaml netapp_ontap botocore==1.31.0 boto3==1.28.0
 python3 -m pip install --upgrade "pywinrm[kerberos]>=0.3.0"
 
 echo ""
